6cf4b27f43f744a5ac06a8c052969493fa0aaa4e
paulr <paulrivera018@gmail.com>
2024-01-12 02:15:11 +0800

    With google map in homepage

--- lib/global/global_var.dart
+++ lib/global/global_var.dart
@@ -1,1 +1,9 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 String userName = "";
+String googleMapKey = "AIzaSyDzF6xmeKRZESFhPXnJ5RELr8N-EuWWo-w";
+
+const CameraPosition googlePlexInitialPosition = CameraPosition(
+  target: LatLng(14.60420000, 120.98220000),
+  zoom: 14.4746,
+);
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../global/global_var.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,13 +14,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+
+  final Completer<GoogleMapController> googleMapCompleterController =  Completer<GoogleMapController>();
+  GoogleMapController? controllerGoogleMap;
+
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Home Page",
-          style: TextStyle(fontSize: 20, color: Colors.white),
-        ),
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationEnabled: true,
+            initialCameraPosition: googlePlexInitialPosition,
+            onMapCreated: (GoogleMapController mapController){
+              controllerGoogleMap = mapController;
+
+              googleMapCompleterController.complete(controllerGoogleMap);
+            },
+          )
+        ],
       ),
     );
   }
