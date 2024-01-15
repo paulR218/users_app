ba697efd2953f1faa9cf572dfc792888e0950879
paulr <paulrivera018@gmail.com>
2024-01-15 19:04:35 +0800

    Implement menu drawer and sign out feature

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,10 @@
 import 'dart:async';
 
+import 'package:users_app/authentication/login_screen.dart';
+
+import '../methods/common_methods.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -18,8 +23,10 @@ class _HomePageState extends State<HomePage> {
 
   final Completer<GoogleMapController> googleMapCompleterController =  Completer<GoogleMapController>();
   GoogleMapController? controllerGoogleMap;
-
   Position? currentPositionOfUser;
+  GlobalKey<ScaffoldState> sKey = GlobalKey<ScaffoldState>();
+  CommonMethods cMethods = CommonMethods();
+
 
   getCurrentLiveLocationOfUser() async {
     Position  positionOfUser = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.bestForNavigation);
@@ -30,13 +37,136 @@ class _HomePageState extends State<HomePage> {
     CameraPosition cameraPosition = CameraPosition(target: LatLngUserPosition, zoom: 15);
 
     controllerGoogleMap!.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+    await getUserInfoAndCheckBlockStatus();
+  }
+
+  getUserInfoAndCheckBlockStatus() async {
+    DatabaseReference usersRef = FirebaseDatabase.instance.ref()
+        .child("users")
+         .child(FirebaseAuth.instance.currentUser!.uid);
+    await usersRef.once().then((snap)
+    {
+      if(snap.snapshot.value != null)
+      {
+        if((snap.snapshot.value as Map)["blockStatus"] == "no")
+        {
+          setState(() {
+            userName = (snap.snapshot.value as Map)["name"];
+          });
+        }
+        else
+        {
+          FirebaseAuth.instance.signOut();
+          Navigator.push(context, MaterialPageRoute(builder: (c) => const LoginScreen()));
+          cMethods.displaySnackbar("Your account is blocked. Contact admin", context);
+        }
+      } else
+      {
+        FirebaseAuth.instance.signOut();
+        Navigator.push(context, MaterialPageRoute(builder: (c) => const LoginScreen()));
+      }
+    });
+
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
+      key:sKey,
+      drawer: Container(
+        width: 230,
+        color: Colors.amber,
+        child: Drawer(
+          backgroundColor: Colors.white10,
+            child: ListView(
+              children: [
+              //header
+
+              Container(
+                  color: Colors.amber,
+                  height: 160,
+                  child: DrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: Colors.amber,
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          "assets/images/avatarman.png",
+                          width: 60,
+                          height: 60,
+                        ),
+
+                        const SizedBox(width : 16,),
+
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              userName,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.indigo
+                              ),
+                            ),
+                            const Text(
+                              "Profile",
+                              style: TextStyle(
+                                color: Colors.indigo,
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+            ),
+
+              const Divider(
+                height: 1,
+                color: Colors.indigo,
+                thickness: 1,
+              ),
+
+              const SizedBox(height: 10,),
+
+              //body
+
+              ListTile(
+                leading: IconButton(
+                    onPressed: (){},
+                    icon: const Icon(Icons.info, color: Colors.indigo,)
+                ),
+                title: const Text("About", style: TextStyle(color: Colors.indigo),),
+              ),
+
+              GestureDetector(
+                onTap: (){
+                  FirebaseAuth.instance.signOut();
+                  Navigator.push(context, MaterialPageRoute(builder: (c) => LoginScreen()));
+                },
+                child: ListTile(
+                  leading: IconButton(
+                      onPressed: (){
+                        FirebaseAuth.instance.signOut();
+                        Navigator.push(context, MaterialPageRoute(builder: (c) => LoginScreen()));
+                      },
+                      icon: const Icon(Icons.logout, color: Colors.indigo,)
+                  ),
+                  title: const Text("Logout", style: TextStyle(color: Colors.indigo),),
+                
+                ),
+              ),
+              ],
+            ),
+       ),
+      ),
       body: Stack(
         children: [
+          //google map
           GoogleMap(
+            padding: const EdgeInsets.only(top: 25),
             mapType: MapType.normal,
             myLocationEnabled: true,
             initialCameraPosition: googlePlexInitialPosition,
@@ -47,7 +177,43 @@ class _HomePageState extends State<HomePage> {
 
               getCurrentLiveLocationOfUser();
             },
-          )
+          ),
+
+          //drawer button or menu button
+          Positioned(
+            top: 36,
+            left: 19,
+            child: GestureDetector(
+              onTap: ()
+              {
+                sKey.currentState!.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: const
+                  [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 5,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7),
+                    )
+                  ]
+                ),
+                child: const CircleAvatar(
+                  backgroundColor: Colors.amber,
+                  radius: 20,
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.indigo,
+                  ),
+                ),
+              ),
+
+            ),
+          ),
         ],
       ),
     );
